50d079b0a7bd9980e1f75cf3ddf38af8d624a70f
shovanpaul48 <shovanpaul48@gmail.com>
2024-06-01 13:40:26 +0530

    vercel.jason added

--- projectmanager/projectmanager/urls.py
+++ projectmanager/projectmanager/urls.py
@@ -27,3 +27,6 @@ urlpatterns = [
 
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
