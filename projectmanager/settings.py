0e0dc2b894824d4d5f5311d97707e6144db59e2b
shovanpaul48 <shovanpaul48@gmail.com>
2024-06-06 11:34:07 +0530

    DEBUG,SECRET_KEY,ALLOWED_HOSTS added on env variables

--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -20,13 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gn@v&vp8svr02@j2q3l$0q$3jm=2w$i8ajn4zmdd!mzt#x_8$w"
+# SECRET_KEY = "django-insecure-gn@v&vp8svr02@j2q3l$0q$3jm=2w$i8ajn4zmdd!mzt#x_8$w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+# ALLOWED_HOSTS = ["*"]
 
-ALLOWED_HOSTS = ["*"]
-
+#### For Render SetUp
+DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Application definition
 
