d36398f3953abe11e857ac4f934f2c16eb029cda
shovanpaul48 <shovanpaul48@gmail.com>
2024-06-06 12:19:51 +0530

    testing 0.3

--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-gn@v&vp8svr02@j2q3l$0q$3jm=2w$i8ajn4zmdd!mzt#x_8$w"
+SECRET_KEY = "django-insecure-gn@v&vp8svr02@j2q3l$0q$3jm=2w$i8ajn4zmdd!mzt#x_8$w"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
 DEBUG = False
 ALLOWED_HOSTS = ["*"]
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Application definition
 
